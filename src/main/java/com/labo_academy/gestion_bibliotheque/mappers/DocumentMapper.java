ff500eccca49eb8e67045eee313dbfa92965d742
PEMBOURA INONA <inonapemboura86@gmail.com>
2025-09-03 13:48:56 +0100

    introduction des mappers

--- src/main/java/com/labo_academy/gestion_bibliotheque/mappers/DocumentMapper.java
+++ src/main/java/com/labo_academy/gestion_bibliotheque/mappers/DocumentMapper.java
@@ -1,4 +1,19 @@
 package com.labo_academy.gestion_bibliotheque.mappers;
 
+import com.labo_academy.gestion_bibliotheque.dto.documentDto.DocumentResponseDto;
+import com.labo_academy.gestion_bibliotheque.entity.Document;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DocumentMapper {
+
+    // Convertion d'une Entity en une DTO
+
+    public DocumentResponseDto fromEntityToDto(Document document){
+        return new DocumentResponseDto(
+                document.get
+        )
+
+    }
+
 }
